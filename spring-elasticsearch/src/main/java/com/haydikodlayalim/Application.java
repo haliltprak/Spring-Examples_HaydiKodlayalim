5034834ce7dde06754731d0bf415fc5eb2ef9376
haliltprak <haliltoprak4713@gmail.com>
2023-01-18 11:01:49 +0300

    hatalar giderildi

--- spring-elasticsearch/src/main/java/com/haydikodlayalim/Application.java
+++ spring-elasticsearch/src/main/java/com/haydikodlayalim/Application.java
@@ -1,13 +1,15 @@
 package com.haydikodlayalim;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@SpringBootApplication
+@EnableElasticsearchRepositories // elastic search repositoryleri kullanmak icin bu notasyonu eklemeliyiz
+public class Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(Application.class, args);
     }
 }
