98594fab12abcc0399f895e6a3dc421797083817
haliltprak <haliltoprak4713@gmail.com>
2023-01-24 16:37:36 +0300

    spring-boot-dockerization modulu olusturuldu ve gerekli dependencyler pom.xml e eklendi. Ornek bir restcontroller classi olusturuldu.

---
+++ spring-boot-dockerization/src/main/java/com/haydikodlayalim/controller/MesajApi.java
@@ -0,0 +1,16 @@
+package com.haydikodlayalim.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mesaj")
+public class MesajApi {
+
+    @GetMapping
+    public String mesajVer(){
+        return "Docker Image icersinden merhaba";
+    }
+
+}
