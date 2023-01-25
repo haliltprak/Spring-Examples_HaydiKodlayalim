f7b6ddea7a3234afe2ed94b7bbdb5d4a169860ab
haliltprak <haliltoprak4713@gmail.com>
2023-01-25 12:59:26 +0300

    spring-boot-aop modulu olusturuldu ve aop icin service, controller siniflari olusturuldu

---
+++ spring-boot-aop/src/main/java/com/haydikodlayalim/aop/service/MessageService.java
@@ -0,0 +1,9 @@
+package com.haydikodlayalim.aop.service;
+
+public class MessageService {
+
+    public String mesajVer(String parametre){
+        System.out.println(parametre);
+        return parametre;
+    }
+}
