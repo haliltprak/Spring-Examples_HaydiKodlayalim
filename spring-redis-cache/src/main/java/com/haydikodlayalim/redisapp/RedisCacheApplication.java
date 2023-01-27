acf9396846f49b132c667fbf96e695ae106923f4
haliltprak <haliltoprak4713@gmail.com>
2023-01-27 15:10:55 +0300

    RedisCacheService te yer alan longRunningMethodunu cacheable yaptik ve RedisCacheApplication classimizda Cachin islemini aktiflestirdik

--- spring-redis-cache/src/main/java/com/haydikodlayalim/redisapp/RedisCacheApplication.java
+++ spring-redis-cache/src/main/java/com/haydikodlayalim/redisapp/RedisCacheApplication.java
@@ -2,8 +2,10 @@ package com.haydikodlayalim.redisapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching // Cache ozelligini aktiflestiriyoruz. Spring in ozelligi bu
 public class RedisCacheApplication
 {
     public static void main( String[] args )
--- spring-redis-cache/src/main/java/com/haydikodlayalim/redisapp/service/RedisCacheService.java
+++ spring-redis-cache/src/main/java/com/haydikodlayalim/redisapp/service/RedisCacheService.java
@@ -1,14 +1,16 @@
 package com.haydikodlayalim.redisapp.service;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RedisCacheService {
 
+    // bu metodun cache edilebilir oldugunu belirtiyoruz. Parametre alabilir bu annotation.
+    // Boylelikle bu metodu ikinci kez cagirmada 5 saniye beklemeye gerek kalmayacak
+    @Cacheable(cacheNames = "mySpecialCache")
     public String longRunningMethod() throws InterruptedException {
-
         Thread.sleep(5000L); // 5 saniye bekletiyoruz. Uzun bir islem surdugunu varsaymak icin
-
         return "method calisti";
     }
 
